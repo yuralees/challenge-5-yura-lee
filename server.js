8d5debbf99ea76eef7bdc758490b8778e69e8019
Yura lee <yuralee@Yuraui-MacBook-Air.local>
2023-03-08 00:05:24 -0500

    commit

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ app.use(express.static('public'));
 app.get('/api/getuser', (req, res) => {
   const data = {
     name: 'Yura Lee',
-    jobTitle: 'Product Designer',
-    interestingFact: 'I love to play the piano and draw digital art',
+    jobTitle:
+      'a product designer whose natural inclination is to think about problems and how things can be improved for a better quility of life',
+    interestingFact: 'I love to these thigns',
     cta: 'Chat with Me!',
   };
+
   res.json(data);
 });
 
